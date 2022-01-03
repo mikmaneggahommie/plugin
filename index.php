38e59f8c321127f65d5f9426fd88b39b7a7c4baa
mikiyas daniel <it.mikiyas.daniel@gmail.com>
2022-01-04 02:00:48 +0300

    added css to the head

--- index.php
+++ index.php
@@ -11,7 +11,29 @@ Text Domain: myplug
 
 */
 add_action('wp_body_open','head');
+function get_names()
+{
+    if( !is_user_logged_in() )
+    {
+        return ' to ' . get_bloginfo('name');
+    }
+    else
+    {
+        $current_user = wp_get_current_user();
+        return $current_user -> user_login;   
+    }
+}
 function head()
 {
-    echo '<h3 class="head">welcome to my site</h3>';
+    echo '<h3 class="head"> Welcome ' . get_names() . '</h3>';
+}
+add_action('wp_print_styles', 'head_css');
+
+function head_css()
+{
+    echo '
+        <style>
+		h3.head {margin: 0; padding: 30px; text-align: center;}
+        </style>
+    ';
 }
